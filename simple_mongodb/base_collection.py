f5f5a4e0724f84b013ba278d1c2b86230cc0aaa3
gandori <marcel.stiebing@web.de>
2024-08-13 21:39:44 +0200

    implement delete_one in BaseCollection and improve Docstrings

--- simple_mongodb/base_collection.py
+++ simple_mongodb/base_collection.py
@@ -49,6 +49,9 @@ class BaseCollection(Exceptions):
         Args:
             where (dict) : A dictionary specifying the criteria for finding the document.
 
+        Returns:
+            Dict[str, Any]: A dictionary representing the document.
+
         Raises:
             NotFoundError: If the document not find.
             FindOneError: If an error occurs while finding the document.
@@ -69,6 +72,10 @@ class BaseCollection(Exceptions):
             skip (int, optional): The number of documents to skip before starting to return results. Defaults to 0.
             limit (int, optional): The maximum number of documents to return. Defaults to 25.
 
+        Returns:
+            List[Dict[str, Any]]: A list of dictionaries, each representing a document.
+
+
         Raises:
             FindOneError: If an error occurs while finding the documents.
         '''
@@ -85,7 +92,7 @@ class BaseCollection(Exceptions):
             document (dict): The document to be inserted into the collection.
 
         Returns:
-            ObjectId: The ObjectId of the inserted document.
+            ObjectId: The _id of the inserted document.
 
         Raises:
             InsertError: If an error occurs while inserting the document.
@@ -107,6 +114,9 @@ class BaseCollection(Exceptions):
             update (dict): A dictionary specifying the fields and values to update in the document.
             upsert (bool, optional): Whether to insert the document if it does not exist. Defaults to False.
 
+        Returns:
+            ObjectId | None: The _id of the inserted document if an upsert took place. Otherwise ``None``.
+
         Raises:
             UpdateError: If an error occurs while updating the document.
             DuplicateKeyError: If the document cannot be inserted due to a duplicate key constraint violation.
@@ -120,11 +130,33 @@ class BaseCollection(Exceptions):
             upsert=upsert,
         )
 
+    async def delete_one(self, where: Dict[str, Any]) -> None:
+        '''
+        Delete one document in the collection
+
+        Args:
+            where (dict): A dictionary specifying the criteria for finding the document to delete.
+
+        Returns:
+            None: This method does not return a value.
+
+        Raises:
+            DeleteError: If an error occurs while deleting the document.
+        '''
+
+        await self.client.delete_one(
+            db=self.db, collection=self.collection, where=where
+        )
+
     async def drop_collection(self) -> None:
         '''
         Drop the collection from the database
 
+        Returns:
+            None: This method does not return a value.
+
         Raises:
             DropCollectionError: if an error occurs while dropping the collection
         '''
+
         await self.client.drop_collection(db=self.db, collection=self.collection)
