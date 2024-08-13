0e8e0b0cd172f2636af0be94effa57adb01c665c
gandori <marcel.stiebing@web.de>
2024-08-13 14:55:33 +0200

    update package main init file

--- simple_mongodb/__init__.py
+++ simple_mongodb/__init__.py
@@ -1,4 +1,11 @@
-from .base_collection import BaseCollection  # type: ignore
-from .index import Index  # type: ignore
-from .mongodb_client import MongoDBClient  # type: ignore
-from .mongodb_client_settings import MongoDBClientSettings  # type: ignore
+from .base_collection import BaseCollection
+from .index import Index
+from .mongodb_client import MongoDBClient
+from .mongodb_client_settings import MongoDBClientSettings
+
+__all__ = [
+    'BaseCollection',
+    'Index',
+    'MongoDBClient',
+    'MongoDBClientSettings',
+]
