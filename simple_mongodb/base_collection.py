19f506c8998bf1a8c9d577656a8f2087fe4a146f
gandori <marcel.stiebing@web.de>
2024-08-15 00:35:09 +0200

    improve docstrings in BaseCollection class

--- simple_mongodb/base_collection.py
+++ simple_mongodb/base_collection.py
@@ -47,15 +47,20 @@ class BaseCollection(Exceptions):
         Find one document in the collection.
 
         Args:
-            where (dict) : A dictionary specifying the criteria for finding the document.
+            where (dict):
+                A dictionary specifying the criteria for finding the document.
 
         Returns:
-            Dict[str, Any]: A dictionary representing the document.
+            document (Dict[str, Any]):
+                A dictionary representing the document.
 
         Raises:
-            NotFoundError: If the document not find.
-            FindOneError: If an error occurs while finding the document.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            NotFoundError:
+                If the document not find.
+            FindOneError:
+                If an error occurs while finding the document.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         return await self.client.find_one(
@@ -69,17 +74,22 @@ class BaseCollection(Exceptions):
         Find documents in the collection.
 
         Args:
-            where (dict): A dictionary specifying the criteria for finding the documents.
-            skip (int, optional): The number of documents to skip before starting to return results. Defaults to 0.
-            limit (int, optional): The maximum number of documents to return. Defaults to 25.
+            where (dict):
+                A dictionary specifying the criteria for finding the documents.
+            skip (int, optional):
+                The number of documents to skip before starting to return results. Defaults to 0.
+            limit (int, optional):
+                The maximum number of documents to return. Defaults to 25.
 
         Returns:
-            List[Dict[str, Any]]: A list of dictionaries, each representing a document.
-
+            Documents (List[Dict[str, Any]]):
+                A list of dictionaries, each representing a document.
 
         Raises:
-            FindOneError: If an error occurs while finding the documents.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            FindOneError:
+                If an error occurs while finding the documents.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         return await self.client.find(
@@ -91,16 +101,20 @@ class BaseCollection(Exceptions):
         Insert one document into the collection.
 
         Args:
-            document (dict): The document to be inserted into the collection.
+            document (dict):
+                The document to be inserted into the collection.
 
         Returns:
-            ObjectId: The _id of the inserted document.
+            ObjectId (ObjectId):
+                The _id of the inserted document.
 
         Raises:
-            InsertError: If an error occurs while inserting the document.
-            DuplicateKeyError: If the document cannot be inserted due to a
-                               duplicate key constraint violation.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            InsertError:
+                If an error occurs while inserting the document.
+            DuplicateKeyError:
+                If the document cannot be inserted due to a duplicate key constraint violation.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         return await self.client.insert_one(
@@ -117,24 +131,27 @@ class BaseCollection(Exceptions):
         Insert many documents in the collection.
 
         Args:
-            documents (List[Dict[str, Any]]): A list of dictionaries, each representing
-                                              a document to be inserted into the collection.
-
-            ordered (bool): If True, documents will be inserted in the order provided,
-                            and the insertion will stop upon encountering an error. If False, the server
-                            will attempt to insert all documents, even if some fail.
-
-            bypass_document_validation (bool): If True, bypasses server-side validation
-                                               for the documents being inserted.
+            documents (List[Dict[str, Any]]):
+                A list of dictionaries, each representing
+                a document to be inserted into the collection.
+            ordered (bool):
+                If True, documents will be inserted in the order provided,
+                and the insertion will stop upon encountering an error. If False, the server
+                will attempt to insert all documents, even if some fail.
+            bypass_document_validation (bool):
+                If True, bypasses server-side validation for the documents being inserted.
 
         Returns:
-            list[ObjectId]: A list of _ids of the inserted documents, in the order provided.
-                            If False is passed for the ordered parameter the server may have inserted
-                            the documents in a different order than what is presented here.
+            ObjectId`s (list[ObjectId]):
+                A list of _ids of the inserted documents, in the order provided.
+                If False is passed for the ordered parameter the server may have inserted
+                the documents in a different order than what is presented here.
 
         Raises:
-            InsertError: If an error occurs while inserting the documents.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            InsertError:
+                If an error occurs while inserting the documents.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         return await self.client.insert_many(
@@ -152,17 +169,24 @@ class BaseCollection(Exceptions):
         Update one document in the collection.
 
         Args:
-            where (dict): A dictionary specifying the criteria for finding the document to update.
-            update (dict): A dictionary specifying the fields and values to update in the document.
-            upsert (bool, optional): Whether to insert the document if it does not exist. Defaults to False.
+            where (dict):
+                A dictionary specifying the criteria for finding the document to update.
+            update (dict):
+                A dictionary specifying the fields and values to update in the document.
+            upsert (bool, optional):
+                Whether to insert the document if it does not exist. Defaults to False.
 
         Returns:
-            ObjectId | None: The _id of the inserted document if an upsert took place. Otherwise ``None``.
+            ObjectId | None:
+                The _id of the inserted document if an upsert took place. Otherwise ``None``.
 
         Raises:
-            UpdateError: If an error occurs while updating the document.
-            DuplicateKeyError: If the document cannot be inserted due to a duplicate key constraint violation.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            UpdateError:
+                If an error occurs while updating the document.
+            DuplicateKeyError:
+                If the document cannot be inserted due to a duplicate key constraint violation.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         return await self.client.update_one(
@@ -178,14 +202,18 @@ class BaseCollection(Exceptions):
         Delete one document in the collection
 
         Args:
-            where (dict): A dictionary specifying the criteria for finding the document to delete.
+            where (dict):
+                A dictionary specifying the criteria for finding the document to delete.
 
         Returns:
-            None: This method does not return a value.
+            None:
+                This method does not return a value.
 
         Raises:
-            DeleteError: If an error occurs while deleting the document.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            DeleteError:
+                If an error occurs while deleting the document.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         await self.client.delete_one(
@@ -197,11 +225,14 @@ class BaseCollection(Exceptions):
         Drop the collection from the database
 
         Returns:
-            None: This method does not return a value.
+            None:
+                This method does not return a value.
 
         Raises:
-            DropCollectionError: if an error occurs while dropping the collection.
-            ServerTimeoutError: Raised if the server takes too long to respond.
+            DropCollectionError:
+                if an error occurs while dropping the collection.
+            ServerTimeoutError:
+                Raised if the server takes too long to respond.
         '''
 
         await self.client.drop_collection(db=self.db, collection=self.collection)
