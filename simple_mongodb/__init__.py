10f1519bee3e8e8412c6c6b558eaf472e32f28fc
gandori <marcel.stiebing@web.de>
2024-09-06 18:33:09 +0200

    add __version__ in package init file

--- simple_mongodb/__init__.py
+++ simple_mongodb/__init__.py
@@ -2,6 +2,7 @@ from .base_collection import BaseCollection
 from .index import Index
 from .mongodb_client import MongoDBClient
 
+__version__ = '0.5.0'
 __all__ = [
     'BaseCollection',
     'Index',
