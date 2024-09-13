ba958b61727eac5606465ffb60720fd222d6f460
Marcel Stiebing <76639087+Gandori@users.noreply.github.com>
2024-09-13 19:48:12 +0200

    0.8.2 (#15)
    * add update_requirements_files.py
    
    * add install_dependencies.py
    
    * add requirements.txt
    
    * add requirements-dev.txt
    
    * add check_versions.py
    
    * update Publish Package action
    
    * update check_versions.py set version as env to use in next steps
    
    * fix issue to set version in check_versions.py
    
    * update env name in check_versions.py to PACKAGE_VERSION
    
    * add check_changelog.py
    
    * add step for changelog check in Publish Package action
    
    * update version in package init file to 0.8.1
    
    * update version to 0.8.2 in pyproject.toml
    
    * update version to 0.8.2 in package init file
    
    * add changelog for 0.8.2

--- simple_mongodb/__init__.py
+++ simple_mongodb/__init__.py
@@ -2,7 +2,7 @@ from .base_collection import BaseCollection
 from .index import Index
 from .mongodb_client import MongoDBClient
 
-__version__ = '0.5.0'
+__version__ = '0.8.2'
 __all__ = [
     'BaseCollection',
     'Index',
